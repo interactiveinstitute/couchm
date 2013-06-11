b3ccea6278e64cf5feb3157ac721ebe41a85cda0
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2013-06-11 11:40:03 +0200

    Make the extremas view more forgiving of faulty timestamps

--- app.js
+++ app.js
@@ -300,14 +300,17 @@ ddoc.views.extrema = {
       var ignore = ['_id', '_rev', 'source', 'type', 'user'];
       for (var name in doc) {
         if (ignore.indexOf(name) == -1) {
-          var key = [doc.source, name, doc.timestamp];
+          var timestamp = parseInt(doc.timestamp);
+          var key = [doc.source, name, timestamp];
           var value = doc[name];
+          timestamp = (timestamp > 10 * 365 * 24 * 60 * 60 * 1000) ? timestamp : timestamp * 1000;
+          if (timestamp > 50 * 365 * 24 * 60 * 60 * 1000) return;
           if (value === true || value === false) {
-            emit(key, [doc.timestamp, +value]);
+            emit(key, [timestamp, +value]);
           } else {
             var parsed = parseFloat(value);
             if (!isNaN(parsed)) {
-              emit(key, [doc.timestamp, parsed]);
+              emit(key, [timestamp, parsed]);
             }
           }
         }
